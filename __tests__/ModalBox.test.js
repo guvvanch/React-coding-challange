01b0c742766871363c0268a01f4f3cc70b5ea8bc
Guvanch G <asd@example.com>
2022-03-28 21:49:16 -0400

    compile repetitive code in one func

--- __tests__/ModalBox.test.js
+++ __tests__/ModalBox.test.js
@@ -5,37 +5,39 @@ import ModalBox from "../components/ModalBox";
 import ModalImage from "../components/ModalImage";
 import { photo } from "../example-photo-data";
 
+const setUp = (props = {}) => {
+  let modalImage = photo;
+  const show = false;
+  const clickClose = jest.fn();
+  const component = shallow(
+    <ModalBox
+      modalImage={modalImage}
+      show={show}
+      clickClose={clickClose}
+      {...props}
+    />
+  );
+  return component;
+};
+
 describe("ModalBox ", () => {
+  let component;
+  beforeEach(() => {
+    component = setUp();
+  });
+
   it("should verify close icon", () => {
-    let modalImage = photo;
-    const show = false;
-    const clickClose = jest.fn();
-    const wrapper = shallow(
-      <ModalBox modalImage={modalImage} show={show} clickClose={clickClose} />
-    );
-    const x_icon = wrapper.find("a");
+    const x_icon = component.find("a");
     expect(x_icon.exists()).toBe(true);
   });
 
   it("should verify that only one x exists", () => {
-    let modalImage = photo;
-    const show = false;
-    const clickClose = jest.fn();
-    const wrapper = shallow(
-      <ModalBox modalImage={modalImage} show={show} clickClose={clickClose} />
-    );
-    const x_icon = wrapper.find("a");
+    const x_icon = component.find("a");
     expect(x_icon.length).toEqual(1);
   });
 
   it("should verify the modalImage component", () => {
-    let modalImage = photo;
-    const show = false;
-    const clickClose = jest.fn();
-    const wrapper = shallow(
-      <ModalBox modalImage={modalImage} show={show} clickClose={clickClose} />
-    );
-    const x_icon = wrapper.find(ModalImage);
-    expect(x_icon.exists()).toBe(true);
+    const modalImageComponent = component.find(ModalImage);
+    expect(modalImageComponent.exists()).toBe(true);
   });
 });
