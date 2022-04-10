05fca94b7fe79d66eaff6843d3d291d3ace8dc75
Guvanch G <asd@example.com>
2022-04-10 11:13:49 -0400

    add styling to searchbar

--- components/SearchBar.js
+++ components/SearchBar.js
@@ -1,16 +1,20 @@
+import styles from "../styles/SearchBar.module.css";
+import SearchIcon from "@mui/icons-material/Search";
+
 export default function SearchBar({ search, searchTerm, handleInput }) {
   return (
-    <div>
-      <form onSubmit={search}>
-        <input
-          className="search"
-          name="search"
-          value={searchTerm}
-          onChange={handleInput}
-          type="text"
-        />
-        <button className="btn-search">Search</button>
-      </form>
-    </div>
+    <form onSubmit={search} className={styles.searchForm}>
+      <input
+        className={styles.searchField}
+        name="search"
+        value={searchTerm}
+        onChange={handleInput}
+        placeholder="Search..."
+        type="text"
+      />
+      <button className={styles.btnSearch}>
+        <SearchIcon />
+      </button>
+    </form>
   );
 }
--- pages/index.js
+++ pages/index.js
@@ -18,7 +18,7 @@ export default function Home({ photos }) {
   const [photoList, setPhotoList] = useState(photos);
   const [searchTerm, setSearchTerm] = useState("");
 
-  //fetches 15 more images upon scrolling
+  //fetches more images upon scrolling
   const getMorePhotos = async (page) => {
     const newPhotos = await fetchPhotos(page, searchTerm);
     setPhotoList((prev) => [...prev, ...newPhotos]); //store all fetched images in photoList state
@@ -36,12 +36,14 @@ export default function Home({ photos }) {
 
   return (
     <div className={styles.container}>
-      <Header />
-      <SearchBar
-        search={search}
-        searchTerm={searchTerm}
-        handleInput={handleInput}
-      />
+      <div className={styles.header}>
+        <Header />
+        <SearchBar
+          search={search}
+          searchTerm={searchTerm}
+          handleInput={handleInput}
+        />
+      </div>
       <Gallery photoList={photoList} getMorePhotos={getMorePhotos} />
     </div>
   );
